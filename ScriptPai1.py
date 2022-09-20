c1ef42b6907db860e32fc5207c18fdde278f68df
pabperort1 <pabperort1@alum.us.es>
2022-09-20 13:36:24 +0200

    Hashea todos los archivos de una carpeta y los almacena en un documento de texto

--- ScriptPai1.py
+++ ScriptPai1.py
@@ -1,3 +1,17 @@
-import hashlib, datetime
+import hashlib, datetime, os
 
-print(datetime.date.today())
+#Ver todos los archivos de una carpeta y los convierte a hash en otro fichero
+
+directorio_ejemplo = '/Users/Pablo/Desktop/UNIVERSIDAD/PAI/PAI1/Carpeta_Para_Hashear'
+lista=[]
+def main():
+    directorio_listado=os.listdir(directorio_ejemplo)
+    for archivo in directorio_listado:
+        hash_value=hashlib.sha256(archivo.encode('utf-8')).hexdigest()
+        lista.append(hash_value)
+    
+    with open("Fichero_conHashes.txt","w") as opfile:
+        opfile.write("\n".join(lista))
+
+if __name__ == '__main__':
+    main()
