c9a009b9b4040cc6b7d1e6c474fb7231caf430c2
pabperort1 <pabperort1@alum.us.es>
2022-09-26 13:49:15 +0200

    Script directorio configurable

--- ScriptPai1.py
+++ ScriptPai1.py
@@ -1,9 +1,18 @@
-import hashlib, datetime, os
-
+import hashlib,  os
+from datetime import datetime
 #Ver todos los archivos de una carpeta y los convierte a hash en otro fichero
 
-directorio_ejemplo = './Carpeta_Para_Hashear'
+
+with open("Config.config") as configfile:
+    directorio=configfile.readline().rstrip()
+print(directorio)
+
 lista=[]
+directorio_ejemplo = directorio.replace("directorio=","")
+print(directorio_ejemplo)
+#fecha_actual=datetime.now()
+#hora_reiniciar=fecha_actual.strftime("%H")
+
 def main():
     directorio_listado=os.listdir(directorio_ejemplo)
     for archivo in directorio_listado:
@@ -14,4 +23,7 @@ def main():
         opfile.write("\n".join(lista))
 
 if __name__ == '__main__':
+    #if hora_reiniciar == 13:
     main()
+    #else:
+        #print("no es la hora")
