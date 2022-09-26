b0efc3db164846c65275d257a6e89073542e0e9a
josedmartin <josedmartinr@gmail.com>
2022-09-26 18:14:41 +0200

    Script ejecutado a una cierta hora y configurable

--- ScriptPai1.py
+++ ScriptPai1.py
@@ -10,23 +10,35 @@ lista=[]
 directorio_elegido = directorio.replace("directorio=","")
 sha_elegido=sha.replace("sha=","")
 
-#fecha_actual=datetime.now()
-#hora_reiniciar=fecha_actual.strftime("%H")
+fecha_actual=datetime.now()
+hora_reiniciar=fecha_actual.strftime("%H")
 
 def main():
     directorio_listado=os.listdir(directorio_elegido)
     for archivo in directorio_listado:
-        dic={"sha224":hashlib.sha224(archivo.encode('utf-8')).hexdigest(),
-        "sha256":hashlib.sha256(archivo.encode('utf-8')).hexdigest(),
-        "sha384":hashlib.sha384(archivo.encode('utf-8')).hexdigest()}
-        hash_value=dic[sha_elegido]
-        lista.append(hash_value)
+
+        dic_def={}
+        dic_def[archivo] = [hashlib.sha224(archivo.encode('utf-8')).hexdigest(),
+        hashlib.sha256(archivo.encode('utf-8')).hexdigest(),
+        hashlib.sha384(archivo.encode('utf-8')).hexdigest(),
+        hashlib.sha512(archivo.encode('utf-8')).hexdigest()]
+
+        if sha_elegido== 'sha224':
+            lista.extend([archivo,dic_def[archivo][0]])
+        elif sha_elegido== 'sha256':
+            lista.extend([archivo,dic_def[archivo][1]])
+        elif sha_elegido== 'sha384':
+            lista.extend([archivo,dic_def[archivo][2]])
+        elif sha_elegido== 'sha512':
+            lista.extend([archivo,dic_def[archivo][3]])
+        else: 
+            print("hash mal escrito")
     
     with open("Fichero_conHashes.txt","w") as opfile:
         opfile.write("\n".join(lista))
 
 if __name__ == '__main__':
-    #if hora_reiniciar == 13:
-    main()
-    #else:
-        #print("no es la hora")
+    if hora_reiniciar == '18':
+        main()
+    else:
+        print("no es la hora")
